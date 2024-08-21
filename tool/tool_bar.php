ed939aa6f74df33f789cf14c176fd73eca08b59a
lebgoodyear <lebgoodyear@protonmail.com>
2024-08-21 16:35:15 +0100

    Fix styling for tool branch
    Add borders to match colour scheme, divide into three sections (one for
    each step), and center text.

--- tool/tool_bar.php
+++ tool/tool_bar.php
@@ -1,7 +1,7 @@
-<div id="tool_bar">
-        <ul>
-            <li id = "upload">Upload csv</li>
-            <li id = "criteria">Choose criteria</li>
-            <li id = "success">Calculate success</li>
-        </ul>
+<div>
+    <ul id="tool_bar">
+        <li id = "upload">Upload csv</li>
+        <li id = "criteria">Choose criteria</li>
+        <li id = "success">Calculate success</li>
+    </ul>
 </div>
