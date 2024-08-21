b019602ff29f950d2d2356c7ffa1ccd29bffb115
lebgoodyear <lebgoodyear@protonmail.com>
2024-08-21 16:10:02 +0100

    Add php file for bar to track progress in tool

---
+++ tool/tool_bar.php
@@ -0,0 +1,7 @@
+<div id="tool_bar">
+        <ul>
+            <li id = "upload">Upload csv</li>
+            <li id = "criteria">Choose criteria</li>
+            <li id = "success">Calculate success</li>
+        </ul>
+</div>
