2226b4dafbe886d60f77b326b5ae2b34906700c5
lebgoodyear <lebgoodyear@protonmail.com>
2024-08-21 13:46:36 +0100

    Add links to home and online tool pages

--- header.php
+++ header.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 
 <header>
-    <div>
-        <img id = "logo" src="/img/logo.png" alt="Chart with four vertical bars and the letters D, A, R and e at the top of each.">
-    </div>
-    <div class="title">
-        <p><strong>D</strong>ecision</p>
-        <p><strong>A</strong>nalysis</p>
-        <p><strong>Re</strong>finement</p>
-    </div>
+    <a class="stamp" href="/index.php">
+            <div>
+                <img id = "logo" src="/img/logo.png" alt="Chart with four vertical bars and the letters D, A, R and e at the top of each.">
+            </div>
+            <div class="title">
+                <p><strong>D</strong>ecision</p>
+                <p><strong>A</strong>nalysis</p>
+                <p><strong>Re</strong>finement</p>
+        </div>
+    </a>
     <nav id="navbar">
         <ul>
             <li>About</li>
