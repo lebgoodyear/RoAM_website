f453f7030e01f691849407a90042d15a6e5bd795
lebgoodyear <lebgoodyear@protonmail.com>
2024-08-21 18:18:54 +0100

    Add check to see if file is csv file

--- tool/csv_formatting.php
+++ tool/csv_formatting.php
@@ -1,21 +1,33 @@
 <?php
 
+ob_start();
+
 session_start();
 
+echo "starting";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Check if a file was uploaded
     if (isset($_FILES['csv_file']) && $_FILES['csv_file']['error'] === UPLOAD_ERR_OK) {
         $csv_file = $_FILES['csv_file']['tmp_name'];
+        $file_info = pathinfo($_FILES['csv_file']['name']);
+        $file_extension = strtolower($file_info['extension']);
 
-        // Save the data to the session
-        $_SESSION['csv_data'] = $csv_file;
+        // Check if the uploaded file is a CSV file
+        if ($file_extension === 'csv') {
 
-        // Redirect to the next page
-        header('Location: select_criteria.php');
-        exit;
+            // Save the data to the session
+            $_SESSION['csv_data'] = $csv_file;
+
+            // Redirect to the next page
+            header('Location: select_criteria.php');
+            exit;
+        }    
+        else {
+            echo "Error: Uploaded file is not a csv.";
+        }
     } else {
-        echo "No file was uploaded.";
+        echo "Error: No file was uploaded.";
     }
 }
 
