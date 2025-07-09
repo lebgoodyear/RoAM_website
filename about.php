2d73d2e42014df09b1dae2316a3d1864442371dd
lebgoodyear <lebgoodyear@protonmail.com>
2025-07-09 16:46:59 +0100

    Update About text and button layout

--- about.php
+++ about.php
@@ -32,21 +32,28 @@
                         <h2>What is it for?</h2>
                     </div>
                     <div class="textbox">
-                        <p>We frequently want to measure success against a particular goal.
-                            The RoAM framework enables tailor-made metrics to measure 
-                            goal achievement across different options.
+                        <p> The RoAM framework is used to to measure success against 
+                            a particular goal and compare across different ways of 
+                            achieving that goal to see which performs best.
+                            It is a custom-built indicator constructed following a particular 
+                            structural framework. It can be used
+                            in any field that requires comparisons of goal achievement across 
+                            heterogenous datasets.
+                        </p>
                         </p>
                     </div>
                 </div>
                 <div class="QA">
                     <div class="questionbox">
-                        <h2>What actually is it?</h2>
+                        <h2>How does it work?</h2>
                     </div>
                     <div class="textbox">
-                        <p>RoAM stands for Root/Additional Metric and is a custom-built indicator
-                            constructed following a particular structural framework. It can be used
-                            in any field that requires comparisons of goal achievement across 
-                            heterogenous datasets.
+                        <p>RoAM stands for Root/Additional Metric. A RoAM is constructed
+                            by choosing criteria that you think are an important part of 
+                            achieving your goal. These criteria are split into 'root', which
+                            are criteria that result in complete failure if they are not met,
+                            and 'additional' which are extra criteria that scale the metric
+                            but do not result in failure if they are not met.
                         </p>
                     </div>
                 </div>
@@ -55,9 +62,19 @@
                         <h2>When is it useful?</h2>
                     </div>
                     <div class="textbox">
+                        <p>The RoAM framework is useful for situations when you want to:</p>
                         <ul>
-                            <li>You want to include factors outside of effectiveness, such as cost</li>
-                            <li>You want to compare across aggragate datasets</li>        
+                            <li>Create a custom metric but don't know where to start
+                            </li>
+                            <li>Encompass many criteria
+                                with differing importance
+                            </li>
+                            <li>Compare across aggragate datasets and don't 
+                                have access to the original datasets
+                            </li>        
+                             <li>Compare goal achievement across 
+                                highly heterogenous datasets
+                            </li>        
                         </ul>
                     </div>
                 </div>
@@ -65,23 +82,33 @@
 
             <div class="aboutButtons">
                 <div class="buttonSet">
-                    <div class="arrows">
+                    <!--<div class="arrows">
                         <img class="indicatorArrow" src="/img/arrow.png">
                         <img class="indicatorArrow" src="/img/arrow.png">
                         <img class="indicatorArrow" src="/img/arrow.png">
-                    </div>
+                        <img class="indicatorArrow" src="/img/arrow.png">
+                        <img class="indicatorArrow" src="/img/arrow.png">
+                    </div>-->
                     <a href="https://arxiv.org/abs/2507.01526">
-                        <button>Click here to read our paper on arXiv</button>
+                        <button>
+                            For more information, read our paper on&nbsp<span class="link">arXiv</span>
+                        </button>
                     </a>
                 </div>
             
                 <div class="buttonSet">
-                    <div class="arrows">
+                    <!--<div class="arrows">
                         <img class="indicatorArrow" src="/img/arrow.png">
                         <img class="indicatorArrow" src="/img/arrow.png">
                         <img class="indicatorArrow" src="/img/arrow.png">
-                    </div>
-                    <button>Click here to see the framework in action</button>
+                        <img class="indicatorArrow" src="/img/arrow.png">
+                        <img class="indicatorArrow" src="/img/arrow.png">
+                    </div>-->
+                    <a>
+                        <button>
+                            To get started, check out our&nbsp<span class="link">tutorial</span>
+                        </button>
+                    </a>
                 </div>
             </div>
         </div>
